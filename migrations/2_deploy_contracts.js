25498ac7c64c23882a62a38efd15e572a3cb74a5
PijiDev <pijidev@gmail.com>
2023-08-13 22:50:36 +0200

    Redeploy with changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Paytr = artifacts.require("Paytr.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Paytr, "0x83C766237dD04EB47F62784218839F892A691E84","0xc3d688B66703497DAA19211EEdff47f25384cdc3",6,"0x399F5EE127ce7432E4921a61b8CF52b0af52cbfE");
+  deployer.deploy(Paytr, "0xc3d688B66703497DAA19211EEdff47f25384cdc3","0xFd55fCd10d7De6C6205dBBa45C4aA67d547AD8F2","0x399F5EE127ce7432E4921a61b8CF52b0af52cbfE",10, 7, 30, 10, 100000000000, 5);//100,000 USDC
 };
