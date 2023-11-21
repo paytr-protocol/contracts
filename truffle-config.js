770bb985b140853851ef37747006713cad958374
pclaesen <pieterclaesen@gmail.com>
2023-11-21 08:33:46 +0100

    Remove gasPrice parameter

--- truffle-config.js
+++ truffle-config.js
@@ -8,8 +8,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*", // match any network
-      gasPrice: 14
+      network_id: "*" // match any network
     },  
   },
   dashboard: {
